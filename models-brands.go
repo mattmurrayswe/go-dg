0ef306c33b018470978267e813780f7dbc7c5f5f
matheusmurraydev <2.0turbocoder@gmail.com>
2024-09-03 13:34:33 -0300

    fix: car list

--- models-brands.go
+++ models-brands.go
@@ -7,21 +7,24 @@ import (
 
 var (
 	dgBrands = []string{
-		"BMW", "Chevrolet", "Dodge", "Ford", "Honda", "Hyundai", "Jaguar", "Kia", "Land Rover",
-		"Lexus", "Maserati", "Mercedez-Benz", "Mini", "Mitsubishi", "Nissan", "Peugeot", "Porsche",
-		"RAM", "Renault", "Subaru", "Suzuki", "Toyota", "Volkswagen", "Volvo",
+		"Alfa Romeo", "Audi", "Bentley", "BMW", "Bugatti", "Chevrolet", "Citroën", "Dodge", "Ferrari", "Ford", "Honda", "Jaguar", 
+		"Kia", "Lamborghini", "Land Rover", "Lexus", "Maserati", "Mclaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", 
+		"Porsche", "RAM", "Subaru", "Toyota", "Volkswagen", "Volvo", 
 	}
 
 	allBrands = []string{
-		"BMW",  "BYD", "Caoa Chery", "Chevrolet", "Chrysler", "Citroën", "Dodge", "Fiat", "Ford",
-		"GMC", "GWM", "Honda", "Hummer", "Hyundai", "Iveco", "JAC", "Jaguar", "Jeep", "Kia",
-		"Land Rover", "Lexus", "Lifan", "Maserati", "Mercedez-Benz", "Mini", "Mitsubishi", "Nissan",
-		"Peugeot", "Porsche", "RAM", "Renault", "Smart", "Subaru", "Suzuki", "Toyota", "Troller",
-		"Volkswagen", "Volvo",
+		"Alfa Romeo", "Americar", "Audi", "Bentley", "BMW", "Bugatti", "Bugway", "BYD", "Caoa Chery", "Chamonix", "Chevrolet", 
+		"Chrysler", "Citroën", "Daihatsu", "Dodge", "Effa", "Engesa", "Ferrari", "Fiat", "Ford", "GMC", "GWM", "Honda", 
+		"Hyundai", "Infiniti", "IVECO", "JAC", "Jaguar", "Jeep", "Kia", "Lamborghini", "Land Rover", "Lexus", "Lifan", 
+		"Maserati", "Mclaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Miura", "Mp Lafer", "Nissan", "Peugeot", "Plymouth", 
+		"Porsche", "RAM", "Renault", "Rivian", "Seat", "Smart", "Ssangyong", "Studebaker", "Subaru", "Suzuki", "Tesla", 
+		"Toyota", "Troller", "Volkswagen", "Volvo", "Willys", 
 	}
 
 	dgOmittedBrands = []string{
-		"GMC", "GWM", "Hummer", "Iveco", "JAC", "Jeep", "Lifan", "Smart", "Troller",
+		"Americar", "Bugway", "BYD", "Caoa Chery", "Chamonix", "Chrysler", "Daihatsu", "Effa", "Engesa", "Fiat", "GMC", 
+		"GWM", "Hyundai", "Infiniti", "IVECO", "JAC", "Jeep", "Lifan", "Miura", "MP Lafer", "Peugeot", "Plymouth", "Renault", 
+		"Rivian", "Seat", "Smart", "Ssangyong", "Studebaker", "Suzuki", "Tesla", "Troller", "Willys", 
 	}
 
 	models = []string{"Corolla", "Mustang", "X5", "C-Class"}
